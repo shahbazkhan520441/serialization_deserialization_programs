e1be807a0614f3af2fb71f23ec333d79ba0ae8b8
shahbazkhan520441 <shahbazkhan520441@gmail.com>
2024-08-09 16:07:43 +0530

    first commit

---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module serilization__deserilization {
+}
---
+++ src/serilization/Student.java
@@ -0,0 +1,19 @@
+package serilization;
+
+public class Student {
+	
+	int studentId;
+	String studentName;
+	int studentage;
+	String studentAddress;
+	public Student(int studentId, String studentName, int studentage, String studentAddress) {
+		super();
+		this.studentId = studentId;
+		this.studentName = studentName;
+		this.studentage = studentage;
+		this.studentAddress = studentAddress;
+	}
+	
+	
+
+}
