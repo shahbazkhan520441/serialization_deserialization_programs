858d212f3082ef7afc5360e5d56515fae95e8d7f
shahbazkhan520441 <shahbazkhan520441@gmail.com>
2024-08-09 17:30:01 +0530

    created feature to deserlize java object

--- src/serilization/Student.java
+++ src/serilization/Student.java
@@ -18,6 +18,42 @@ public class Student implements Serializable{
 		this.studentAddress = studentAddress;
 	}
 
+	public int getStudentId() {
+		return studentId;
+	}
+
+	public void setStudentId(int studentId) {
+		this.studentId = studentId;
+	}
+
+	public String getStudentName() {
+		return studentName;
+	}
+
+	public void setStudentName(String studentName) {
+		this.studentName = studentName;
+	}
+
+	public int getStudentage() {
+		return studentage;
+	}
+
+	public void setStudentage(int studentage) {
+		this.studentage = studentage;
+	}
+
+	public String getStudentAddress() {
+		return studentAddress;
+	}
+
+	public void setStudentAddress(String studentAddress) {
+		this.studentAddress = studentAddress;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
 
 
 }
